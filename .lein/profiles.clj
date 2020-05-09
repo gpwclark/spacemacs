e261c1f7578c076350820bf047c03cc8eb339ece
price <price@eucleo.com>
2020-05-09 22:02:52 +0000

    autocommit

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,4 @@
-{:repl {:plugins [[cider/cider-nrepl "0.22.0-beta8"]
-                  [refactor-nrepl "2.4.0-SNAPSHOT"]]
+{:repl {:plugins [[cider/cider-nrepl "0.24.0"]
+                  [refactor-nrepl "2.5.0"]]
         :dependencies [[alembic "0.3.2"]
                        [org.clojure/tools.nrepl "0.2.13"]]}}
