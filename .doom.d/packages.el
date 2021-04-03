48f70c4dbc7b047a602f0e3bd018a77794b8cb24
price <price@eucleo.com>
2021-04-03 20:17:22 +0000

    updates

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -8,6 +8,8 @@
 
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;(package! some-package)
+(package! rainbow-delimiters)
+(package! graphviz-dot-mode)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
