81d76ef05dc4f968f6008a313bb5947cf17448a8
price <price@eucleo.com>
2021-04-08 15:47:48 +0000

    autocommit

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -111,7 +111,7 @@
        :lang
        ;;agda              ; types of types of types of types...
        ;;cc                ; C/C++/Obj-C madness
-       clojure           ; java with a lisp
+       (clojure +lsp)      ; java with a lisp https://emacs-lsp.github.io/lsp-mode/page/lsp-clojure/
        common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
